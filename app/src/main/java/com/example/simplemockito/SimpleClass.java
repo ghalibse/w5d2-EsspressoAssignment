019161c97bcb0fec06779e24979f24c4a8323de3
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-30 12:02:42 -0400

    Add SimpleClass

---
+++ app/src/main/java/com/example/simplemockito/SimpleClass.java
@@ -0,0 +1,19 @@
+package com.example.simplemockito;
+
+import android.content.Context;
+
+/**
+ * Created by evin on 4/29/16.
+ */
+public class SimpleClass {
+
+    Context mContext;
+
+    SimpleClass(Context context) {
+        mContext = context;
+    }
+
+    public String getHelloWorldString() {
+        return mContext.getString(R.string.hello_word) + "b";
+    }
+}
